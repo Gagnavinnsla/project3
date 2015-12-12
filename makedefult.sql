76f65d194fa364e93706959df3532a1c6b66a11f
davidhal14 <davidhall14@ru.is>
2015-12-12 14:11:03 +0000

    lol

--- makedefult.sql
+++ makedefult.sql
@@ -28,15 +28,30 @@ timi int references fasteignaverd(timi)
 
 create table utskrifadirhaskola(
 timi int references fasteignaverd(timi),
-19ogyngri int,
+'yngri' int,
 20-21 int,
 22-24 int,
 25-29 int
 );
 
 create table leiguverd(
-ibudir int,
 timi int references fasteignaverd(timi),
-kostnm2 int,
-voxtur float
+studiomedalverd int,
+herb2medalverd int,
+herb3medalverd int,
+herb4medalverd int,
+herb5medalverd int,
+herb6medalverd int,
+studiostaerd int,
+herb2steard int,
+herb3steard int,
+herb4steard int,
+herb5steard int,
+herb6steard int,
+studiossamning int,
+herb2samning int,
+herb3samning int,
+herb4samning int,
+herb5samning int,
+herb6samning int
 );
