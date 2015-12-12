44865325afe0cecc75a599f9a3e578f86644964d
davidhal14 <davidhall14@ru.is>
2015-12-12 11:39:57 +0000

    maker of defule

--- makedefult.sql
+++ makedefult.sql
@@ -1,41 +1,43 @@
-create table mannfjoldispa(
-
-);
-
 create table fasteignaverd(
-ibudir
-timi
-kostnm2
+ibudir int,
+timi int,
+kostnm2 int,
 primary key(timi)
 );
 
+create table mannfjoldispa(
+timi int references fasteignaverd(timi),
+spa int
+);
+
 create table erlendirferda (
-fjoldi
-voxtur
-timi references fasteignaverd(timi)
+fjoldi int,
+voxtur int,
+timi int references fasteignaverd(timi)
 );
 
 create table mannfjoldi(
-timi references fasteignaverd(timi)
-mannfjoldi 1.jan
+timi int references fasteignaverd(timi),
+mannfjoldi int
 );
 
 create table leiguverd(
-ibudir
-timi references fasteignaverd(timi)
-kostnm2
-voxtur
+ibudir int,
+timi int references fasteignaverd(timi),
+kostnm2 int,
+voxtur float
 );
 
-create table ungt folk(
-fjoldi
-voxtur
-timi references fasteignaverd(timi)
+create table ungtfolk(
+fjoldi int
+voxtur float 
+timi int references fasteignaverd(timi)
 );
 
 create table haskolanam(
-laun
-kostnadur
-voxtur
-timi references fasteignaverd(timi)
+laun int ,
+kostnadur int,
+voxtur int ,
+timi int references fasteignaverd(timi)
 );
+
